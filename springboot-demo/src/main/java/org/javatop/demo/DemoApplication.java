d16615754f48653c8c8ef371314a643906901fbe
gaoziman <2942894660@qq.com>
2023-12-22 23:19:54 +0800

    配置文件加载顺序

--- springboot-demo/src/main/java/org/javatop/demo/DemoApplication.java
+++ springboot-demo/src/main/java/org/javatop/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 
 package org.javatop.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,12 +15,20 @@ import org.springframework.core.env.Environment;
  * @description : 启动类
  */
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
         Environment environment = context.getBean(Environment.class);
 
         System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
         System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功 ლ(´ڡ`ლ)ﾞ \n");
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("配置名称：" + name);
     }
 }
