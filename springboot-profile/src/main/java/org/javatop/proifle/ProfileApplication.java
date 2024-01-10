62b9c4a2a630987e67b8c195f52cdb66411f69a2
gaoziman <2942894660@qq.com>
2024-01-10 20:57:54 +0800

    profile分支代码练习

---
+++ springboot-profile/src/main/java/org/javatop/proifle/ProfileApplication.java
@@ -0,0 +1,26 @@
+
+package org.javatop.proifle;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-12-12 19:19
+ * @description : 启动类
+ */
+@SpringBootApplication
+public class ProfileApplication{
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ProfileApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功 ლ(´ڡ`ლ)ﾞ \n");
+    }
+}
