b81b9eb10efd3c0627ae3dc0c4bd889abf0f33f7
gaoziman <2942894660@qq.com>
2024-03-07 22:47:06 +0800

    去除多余空白

--- springboot-mapstruct/src/main/java/org/javatop/mapstruct/mapper/StudentMapper.java
+++ springboot-mapstruct/src/main/java/org/javatop/mapstruct/mapper/StudentMapper.java
@@ -7,8 +7,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
